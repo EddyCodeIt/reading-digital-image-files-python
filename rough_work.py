1e1374864a658adeac5bee982c0106ff334d5285
EddyCodeIt <g00315079@gmit.ie>
2017-09-25 16:31:52 +0100

    Read images from file function

--- rough_work.py
+++ rough_work.py
@@ -25,7 +25,40 @@ def read_labels_from_file(filename):
         labels = [myFile.read(1) for i in range(no_lbl)] # read labels byte by byte
         labels = [int.from_bytes(label, 'big') for label in labels] # convert into ints
 
-        return labels
+    return labels
 
 train_labels = read_labels_from_file('data/train-labels-idx1-ubyte.gz')
 test_labels = read_labels_from_file('data/t10k-labels-idx1-ubyte.gz')
+
+
+def read_images_from_file(filename):
+    with gzip.open(filename,'rb') as myFile:
+        magicNum = myFile.read(4)
+        print("Magic Number: ",int.from_bytes(magicNum, 'big'))
+
+        noimg = myFile.read(4)
+        noimg = int.from_bytes(noimg, 'big') 
+        print("Number of images: ", noimg)
+
+        norow = myFile.read(4)
+        norow = int.from_bytes(norow, 'big') 
+        print("Number of rows: ", norow)
+
+        nocol = myFile.read(4)
+        nocol = int.from_bytes(nocol, 'big') 
+        print("Number of columns: ", nocol)
+        
+        images = []
+
+        for i in range(noimg):
+            rows = []
+            for r in range(norow):
+                cols = []
+                for c in range(nocol):
+                    cols.append(int.from_bytes(myFile.read(1), 'big'))
+                rows.append(cols)
+            images.append(rows)
+    return images
+
+train_images = read_images_from_file('data/train-images-idx3-ubyte.gz')
+test_images = read_images_from_file('data/t10k-images-idx3-ubyte.gz')
