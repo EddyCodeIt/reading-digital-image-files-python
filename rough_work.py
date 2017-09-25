4467fdb0390fcad94cf1968297835ed16af8e3e4
EddyCodeIt <g00315079@gmit.ie>
2017-09-25 15:40:37 +0100

    Reading Labels from Training Set Image File

--- rough_work.py
+++ rough_work.py
@@ -1,7 +1,9 @@
 import gzip, struct
 
 myFile = gzip.open('data/t10k-images-idx3-ubyte.gz','rb')
-
+# pointer position starts at start.
+# everytime .read() function is called, pointer possition 
+# stays where .read() stoped. 
 magicNum = myFile.read(4)
 
 #  firstbyte var would print as: b'\x00\x00\x08\x03'
@@ -13,4 +15,7 @@ print(magicNum)
 print(int.from_bytes(magicNum, byteorder ='big'))
 
 # each image is 28 by 28 bytes, 10 000 images in a file + bytes for meta date about a file
-print(28*28*10000+4+4+4+4)
+# print(28*28*10000+4+4+4+4)
+
+no_lbl = myFile.read(4)
+print("Number of labels: ", int.from_bytes(no_lbl, 'big'))
